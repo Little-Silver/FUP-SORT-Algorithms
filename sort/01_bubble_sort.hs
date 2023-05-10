9d2672bfde9e3e34853e5d8f7c6381a6bd18ae1e
islikpas <islikpas@students.zhaw.ch>
2023-05-10 17:40:00 +0200

    improve bubble

--- sort/01_bubble_sort.hs
+++ sort/01_bubble_sort.hs
@@ -5,7 +5,7 @@ import System.Random
 randomIntList :: Int -> Int -> Int -> [Int]
 randomIntList n minVal maxVal = take n $ randomRs (minVal, maxVal) (mkStdGen 42)
 
-list1 = randomIntList 900 0 1000
+list1 = reverse [0..1000] --randomIntList 900 0 1000
 --------------------------------------------------------------------------------------------
 -- Bubble sort -- Version 1 ----------------------------------------------------------------
 --------------------------------------------------------------------------------------------
@@ -55,18 +55,17 @@ bsort3 list = bsort3' True [] list
 -- Bubble sort -- Version 4 ----------------------------------------------------------------
 --------------------------------------------------------------------------------------------
 bsort4 :: Ord a => [a] -> [a]   
-bsort4 list = bsort3' False [] (reverse list)
+bsort4 list = bsort4' False [] list
     where 
-        bsort3' :: Ord a => Bool -> [a] -> [a] -> [a]   
-        bsort3' False s [x] = reverse (x:s)
-        bsort3' True s [x]  = bsort3' False [] (reverse (x:s))
-        bsort3' flag s (x:y:xs) | x > y      = bsort3' True (y:s) (x:xs)
-                                | otherwise  = bsort3' flag (x:s) (y:xs)
+        bsort4' :: Ord a => Bool -> [a] -> [a] -> [a]   
+        bsort4' False s [x] = reverse (x:s)
+        bsort4' True s [x]  = bsort4' False [] (reverse (x:s))
+        bsort4' flag s (x:y:xs) | x > y      = bsort4' True (y:s) (x:xs)
+                                | otherwise  = bsort4' flag (x:s) (y:xs)
 
 --------------------------------------------------------------------------------------------
 -- Bubble sort -- Version 5 ----------------------------------------------------------------
 --------------------------------------------------------------------------------------------
-
 bsort5 :: Ord a => [a] -> [a]
 bsort5 [] = []
 bsort5 s = case bsort5' [] s of
@@ -75,7 +74,7 @@ bsort5 s = case bsort5' [] s of
   where 
         bsort5' :: Ord a => [a] -> [a] -> [a]
         bsort5' s (x:y:xs) | x > y    = bsort5' (y:s) (x:xs)
-                         | otherwise = bsort5' (x:s) (y:xs)
+                           | otherwise = bsort5' (x:s) (y:xs)
         bsort5' s (a) = reverse(a ++ s)
 
 
@@ -90,6 +89,6 @@ run f l fname = do
 main = do
     run bsort1 list1 "bsort1 - Thomas"
     run bsort2 list1 "bsort2 - ChatGPT"
-    run bsort3 list1  "bsort3 - Pascal: Tail Recursive 1"
-    run bsort4 list1  "bsort4 - Pascal: Tail Recursive 2"
-    run bsort5 list1  "bsort5 - Pascal: Tail Recursive 3"
+    run bsort3 list1 "bsort3 - Pascal: Tail Recursive 1"
+    run bsort4 list1 "bsort4 - Pascal: Tail Recursive 2"
+    run bsort5 list1 "bsort5 - Pascal: Tail Recursive 3"
