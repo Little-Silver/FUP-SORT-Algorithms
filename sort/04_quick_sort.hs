332d1e8ea4112a27a8f230ef2cef6baf7a3758c3
islikpas <islikpas@students.zhaw.ch>
2023-05-13 08:09:03 +0200

    Add quicksort implementations

--- sort/04_quick_sort.hs
+++ sort/04_quick_sort.hs
@@ -9,15 +9,33 @@ list1 = randomIntList 5000 0 100
 --------------------------------------------------------------------------------------------
 -- Quick sort -- Version 1 ----------------------------------------------------------------
 --------------------------------------------------------------------------------------------
-qsort :: Ord a => [a] -> [a]
-qsort [] = []
-qsort (x:xs) = qsort [a | a <- xs, a < x]
+qsort1 :: Ord a => [a] -> [a]
+qsort1 [] = []
+qsort1 (x:xs) = qsort1 [a | a <- xs, a < x]
                       ++ [x] ++
-               qsort [b | b <- xs, b >= x]
+               qsort1 [b | b <- xs, b >= x]
                
 --------------------------------------------------------------------------------------------
 -- Quick sort -- Version 2 ----------------------------------------------------------------
 --------------------------------------------------------------------------------------------
+qsort2 :: Ord a => [a] -> [a]
+qsort2 xs = qsort2' xs []
+
+qsort2' :: Ord a => [a] -> [a] -> [a]
+qsort2' [] acc = acc
+qsort2' (x:xs) acc = qsort2' smallerOrEqual (x : qsort2' larger acc)
+  where
+    smallerOrEqual = filter (<= x) xs
+    larger = filter (> x) xs
+
+--------------------------------------------------------------------------------------------
+-- Quick sort -- Version 3 ----------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+qsort3 :: Ord a => [a] -> [a]
+qsort3 (x:xs) = qsort3 (filter (<= x) xs) 
+                     ++ [x] ++ 
+                qsort3 (filter (> x) xs)
+qsort3 x = x
 
 run f l fname = do
     start <- getCurrentTime
@@ -28,5 +46,6 @@ run f l fname = do
 
 
 main = do
-    run qsort list1 "qsort"
-    run qsort list1 "qsort"
+    run qsort1 list1 "Simple Implementation 1"
+    run qsort2 list1 "Tail Recursive"
+    run qsort3 list1 "Simple Implementation 2"
