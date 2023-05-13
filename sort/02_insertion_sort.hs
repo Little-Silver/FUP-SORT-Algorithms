5e3262e7df628b1d2555aecc2e18fe92f0cf850c
islikpas <islikpas@students.zhaw.ch>
2023-05-13 09:13:15 +0200

    add insertion sort implementation

--- sort/02_insertion_sort.hs
+++ sort/02_insertion_sort.hs
@@ -5,24 +5,37 @@ import System.Random
 randomIntList :: Int -> Int -> Int -> [Int]
 randomIntList n minVal maxVal = take n $ randomRs (minVal, maxVal) (mkStdGen 42)
 
-list1 = randomIntList 500 0 100
+list1 = randomIntList 5000 0 100
 --------------------------------------------------------------------------------------------
 -- Insertion sort -- Version 1 ----------------------------------------------------------------
 --------------------------------------------------------------------------------------------
+isort1 :: Ord a => [a] -> [a]
+isort1 [] = []
+isort1 (x:xs) = insert x (isort1 xs)
+
 insert :: Ord a => a -> [a] -> [a]
 insert x [] = [x]
-insert x (y:ys) | x < y     = x:y:ys
+insert x (y:ys) | x <= y     = x:y:ys
                 | otherwise = y:(insert x ys)
-
-isort :: Ord a => [a] -> [a]
-isort [] = []
-isort (x:xs) = insert x (isort xs)
 
 -- ghci> isort list1               
 
 --------------------------------------------------------------------------------------------
 -- Insertion sort -- Version 2 ----------------------------------------------------------------
 --------------------------------------------------------------------------------------------
+isort2 :: Ord a => [a] -> [a]
+isort2 xs = isort2' xs []
+
+isort2' :: Ord a => [a] -> [a] -> [a]
+isort2' [] acc = acc
+isort2' (x:xs) acc = isort2' xs (insert2 x acc)
+
+insert2 :: Ord a => a -> [a] -> [a]
+insert2 x [] = [x]
+insert2 x (y:ys)
+  | x <= y    = x : y : ys
+  | otherwise = y : insert2 x ys
+
 
 -- ghci> bsort1 list2
 run f l fname = do
@@ -34,5 +47,5 @@ run f l fname = do
 
 
 main = do
-    run isort list1 "isort"
-    run isort list1 "isort"
+    run isort1 list1 "isort"
+    run isort2 list1 "isort"
