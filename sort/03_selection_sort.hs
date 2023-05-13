aa12cf07b0b337c1d51c24ce06aebdd9696ba0c4
islikpas <islikpas@students.zhaw.ch>
2023-05-13 08:32:53 +0200

    add selection sort implementation

--- sort/03_selection_sort.hs
+++ sort/03_selection_sort.hs
@@ -6,20 +6,27 @@ import Data.List (delete, minimum)
 randomIntList :: Int -> Int -> Int -> [Int]
 randomIntList n minVal maxVal = take n $ randomRs (minVal, maxVal) (mkStdGen 42)
 
-list1 = randomIntList 500 0 100
+list1 = randomIntList 5000 0 1000
 --------------------------------------------------------------------------------------------
 -- Selection sort -- Version 1 ----------------------------------------------------------------
 --------------------------------------------------------------------------------------------
-ssort :: Ord t => [t] -> [t]
-ssort [] = []
-ssort xs = let { x = minimum xs } 
-           in  x : ssort (delete x xs)
+ssort1 :: Ord t => [t] -> [t]
+ssort1 [] = []
+ssort1 xs = let { x = minimum xs } 
+           in  x : ssort1 (delete x xs)
 
--- ghci> ssort list1               
+-- ghci> ssort1 list1               
 
 --------------------------------------------------------------------------------------------
 -- Selection sort -- Version 2 ----------------------------------------------------------------
 --------------------------------------------------------------------------------------------
+ssort2 :: Ord t => [t] -> [t]
+ssort2 xs = ssort2' [] xs
+
+ssort2' :: Ord t => [t] -> [t] -> [t]
+ssort2' xs [] = reverse xs
+ssort2' xs ys = let {y = minimum ys } 
+                in ssort2' (y:xs) (delete y ys)
 
 
 -- ghci> bsort1 list2
@@ -32,5 +39,5 @@ run f l fname = do
 
 
 main = do
-    run ssort list1 "ssort"
-    run ssort list1 "ssort"
+    run ssort1 list1 "ssort1"
+    run ssort2 list1 "ssort2"
